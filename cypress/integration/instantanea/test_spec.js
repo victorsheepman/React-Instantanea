063afce92ba7580324f9348b9f8a660697fbb5b6
unknown <chirlychplin@gmail.com>
2022-01-13 16:14:44 -0400

    fixed bug sidebar

---
+++ cypress/integration/instantanea/test_spec.js
@@ -0,0 +1,16 @@
+
+describe('instantanea', function () {
+    it('Funcionamiento de rutas y Modales', function () {
+        cy.visit('/')
+        /*cy.get('#historiaBtn').contains('Siguiente').click()
+        cy.get('button').contains('Entrar').click()
+        cy.get('div').contains('Rocky').click()
+        cy.get('.modalPet__exit').click()
+        cy.get('nav').contains('Favoritos').click()
+        cy.get('nav').contains('Mensajes').click()
+        cy.get('.chat').click()
+        cy.get('.modaluser__exit').click()
+        cy.get('nav').contains('Perfil').click()
+        cy.get('button').contains('Salir').click()*/
+    })
+})
