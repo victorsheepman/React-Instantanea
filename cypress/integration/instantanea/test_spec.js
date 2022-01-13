2527cf192187de693af39fcc135a9d2500ae3d55
unknown <chirlychplin@gmail.com>
2022-01-13 16:54:26 -0400

    fixed bug sidebar

--- cypress/integration/instantanea/test_spec.js
+++ cypress/integration/instantanea/test_spec.js
@@ -5,12 +5,5 @@ describe('instantanea', function () {
         cy.get('#historia').contains('Tu Historia').click()
         cy.get('#exit_modal').click()
         cy.get('h4').contains('wendy').click()
-        cy.get('#principal').click()
-        /*cy.get('nav').contains('Favoritos').click()
-        cy.get('nav').contains('Mensajes').click()
-        cy.get('.chat').click()
-        cy.get('.modaluser__exit').click()
-        cy.get('nav').contains('Perfil').click()
-        cy.get('button').contains('Salir').click()*/
     })
 })
